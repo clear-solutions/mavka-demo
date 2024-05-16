918401cd4caa10f92ad382533a49b1ded8394569
Max Danylenko <max.danylenko@clear-solutions.ltd>
2024-05-16 12:28:05 +0200

    init project

---
+++ src/test/java/io/mavka/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mavka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
