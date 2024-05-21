48a5bbbd99b106f19a3d60657c9fb6f036b70900
Max Danylenko <max.danylenko@clear-solutions.ltd>
2024-05-21 15:43:51 +0200

    added Concurrent Modification Exception

--- src/main/java/io/mavka/demo/collection/ConcurrentModificationException.java
+++ src/main/java/io/mavka/demo/collection/ConcurrentModificationException.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RemovingData {
+public class ConcurrentModificationException {
 
 
     public static void main(String[] args) {
